b37e892f61c6c97dcc0d042c940938debec27ce3
BeZaBel <azamorabell@gmail.com>
2023-02-24 17:19:14 -0600

    major mods

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,11 +3,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 killall -q polybar
 
-# polybar -c $SCRIPTPATH/polybar.config onebar 2>&1 | tee -a /tmp/polybar.log & disown
-polybar -c $SCRIPTPATH/polybar.config windownames 2>&1 | tee -a /tmp/polybar.log & disown
-polybar -c $SCRIPTPATH/polybar.config workspaces 2>&1 | tee -a /tmp/polybar.log & disown
-polybar -c $SCRIPTPATH/polybar.config volume 2>&1 | tee -a /tmp/polybar.log & disown
-polybar -c $SCRIPTPATH/polybar.config cpu 2>&1 | tee -a /tmp/polybar.log & disown
-polybar -c $SCRIPTPATH/polybar.config trayonly 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -c $SCRIPTPATH/polybar.config single 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
