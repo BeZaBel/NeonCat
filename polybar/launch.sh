ae6fd03d96c32818e7f2ef4400a6c99b58ee1332
BeZaBel <azamorabell@gmail.com>
2023-04-10 23:43:53 -0600

    alacritty and polybar specific configs

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,8 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 killall -q polybar
 
 polybar -c $SCRIPTPATH/polybar.config single 2>&1 | tee -a /tmp/polybar.log & disown
+# Blank bar for xfce
+# polybar -c $SCRIPTPATH/polybar.config blank 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
+
