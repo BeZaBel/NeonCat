7713285b74ac00482a87806b88a388d0359dd2de
BeZaBel <azamorabell@gmail.com>
2023-04-14 23:30:24 -0600

    polybar modifications of the utilities bar, a new blank bar to make a margin at the bottom and alacritty font change to meslolgl nerd font so tide fish prompt looks good

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,7 @@ killall -q polybar
 polybar -c $SCRIPTPATH/polybar.config single 2>&1 | tee -a /tmp/polybar.log & disown
 # Blank bar for xfce
 polybar -c $SCRIPTPATH/polybar.config blank 2>&1 | tee -a /tmp/polybar.log & disown
+polybar -c $SCRIPTPATH/polybar.config spacer 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 
